c23818a9fd42c11f8b48bc83666ad510e4f036cc
Michael G. Schwern <schwern@pobox.com>
2017-12-04 22:57:22 -0800

    Day 2 part 2.

--- 2017/day02/src/main.rs
+++ 2017/day02/src/main.rs
@@ -1,7 +1,10 @@
+extern crate itertools;
+
 use std::fs::File;
 use std::env;
 use std::io::BufReader;
 use std::io::prelude::*;
+use itertools::Itertools;
 
 #[cfg(test)]
 mod tests {
@@ -16,7 +19,25 @@ mod tests {
         ];
         assert_eq!( checksum(sheet), 18 );
     }
+    
+    #[test]
+    fn test_part_two() {
+        let sheet = vec![
+            vec![5,9,2,8],
+            vec![9,4,7,3],
+            vec![3,8,6,5],
+        ];
+        assert_eq!( checksum2(sheet), 9 );
+    }
+    
+    #[test]
+    fn test_checksum2_row() {
+        assert_eq!( checksum2_row(vec![5,9,2,8]), 4 );
+        assert_eq!( checksum2_row(vec![9,4,7,3]), 3 );
+        assert_eq!( checksum2_row(vec![3,8,6,5]), 2 );
+    }
 }
+
 
 fn checksum( sheet : Vec<Vec<i32>> ) -> i32 {
     let mut sum = 0;
@@ -24,6 +45,28 @@ fn checksum( sheet : Vec<Vec<i32>> ) -> i32 {
         sum += row.iter().max().unwrap() - row.iter().min().unwrap();
     }
     return sum;
+}
+
+fn checksum2( sheet : Vec<Vec<i32>> ) -> i32 {
+    let mut sum = 0;
+    for row in sheet {
+        sum += checksum2_row(row);
+    }
+    return sum;
+}
+
+fn checksum2_row( row : Vec<i32> ) -> i32 {
+    let combos = Itertools::combinations( row.into_iter(), 2 );
+    for combo in combos {
+        if combo[0] % combo[1] == 0 {
+            return combo[0] / combo[1];
+        }
+        else if combo[1] % combo[0] == 0 {
+            return combo[1] / combo[0];
+        }
+    }
+    
+    return 0;
 }
 
 fn read_sheet( fh : File ) -> Vec<Vec<i32>> {
@@ -46,5 +89,5 @@ fn main() {
 
     let sheet = read_sheet(fh);
     
-    println!("{}", checksum(sheet));
+    println!("{}", checksum2(sheet));
 }
