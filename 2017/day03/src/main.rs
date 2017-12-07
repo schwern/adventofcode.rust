13e2474adb12a911b7c7c84a7e26e229b1d9010d
Michael G. Schwern <schwern@pobox.com>
2017-12-07 00:27:46 -0800

    Day 3 Part 1
    I couldn't figure out how to subtract unsigned integers without
    putting them into an even larger signed integer type. So
    distance_from_middle_of_side() and manhattan_distance()
    both return i64s because they're working with u32 even though a u16
    would be fine.

---
+++ 2017/day03/src/main.rs
@@ -0,0 +1,159 @@
+use std::error::Error;
+use std::fmt;
+use std::env;
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+
+    #[test]
+    fn test_get_manhattan_distance() {
+        let mut tests : HashMap<u32,i64> = HashMap::new();
+        tests.insert(1, 0);
+        tests.insert(12,3);
+        tests.insert(23,2);
+        tests.insert(1024,31);
+        
+        for (arg, want) in tests {
+            assert_eq!(Ring::manhattan_distance( arg ), want );
+        }
+    }
+    
+    #[test]
+    fn test_get_ring() {
+        let mut tests : HashMap<u32,Ring> = HashMap::new();
+        tests.insert(1, Ring{num:0,start:1,end:1,size:1});
+        tests.insert(2, Ring{num:1,start:2,end:9,size:3});
+        tests.insert(9, Ring{num:1,start:2,end:9,size:3});
+        tests.insert(10, Ring{num:2,start:10,end:25,size:5});
+        tests.insert(25, Ring{num:2,start:10,end:25,size:5});
+        tests.insert(26, Ring{num:3,start:26,end:49,size:7});
+        
+        for (arg, want) in tests {
+            assert_eq!( Ring::for_square(arg), want );
+        }
+    }
+    
+    #[test]
+    fn test_is_in_ring() {
+        let mut ring = Ring::zero();
+        assert!( ring.is_in_ring(1) );
+        assert!( !ring.is_in_ring(2) );
+        
+        ring.next_ring();
+        assert!( !ring.is_in_ring(1) );
+        assert!( ring.is_in_ring(2) );
+        assert!( ring.is_in_ring(9) );
+        assert!( !ring.is_in_ring(10) );
+    }
+    
+    #[test]
+    fn test_distance_from_middle_of_side_not_in_ring() {
+        let ring = Ring::zero();
+        assert_eq!( ring.distance_from_middle_of_side(2), Err(RingError::NotInRing) );
+    }
+    
+    #[test]
+    fn test_distance_from_middle_of_side() {
+        let mut ring = Ring::zero();
+        ring.next_ring();
+        ring.next_ring();
+        
+        let mut tests = HashMap::new();
+        tests.insert( 11, 0 );
+        tests.insert( 10, 1 );
+        tests.insert( 12, 1 );
+        tests.insert( 13, 2 );
+        tests.insert( 14, 1 );
+        tests.insert( 15, 0 );
+        tests.insert( 16, 1 );
+        tests.insert( 17, 2 );
+        
+        for (arg, want) in tests {
+            assert_eq!( ring.distance_from_middle_of_side(arg).unwrap(), want );
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Ring {
+    num: u16,
+    start: u32,
+    end: u32,
+    size: u16,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum RingError {
+    NotInRing,
+}
+
+impl Error for RingError {
+    fn description(&self) -> &str {
+        match *self {
+            RingError::NotInRing    => "square not in this ring",
+        }
+    }
+}
+
+impl fmt::Display for RingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RingError::NotInRing    => write!(f, "square not in this ring"),
+        }
+    }
+}
+
+impl Ring {
+    fn zero() -> Ring {
+        return Ring{ num: 0, start: 1, end: 1, size: 1 };
+    }
+    
+    fn next_ring( &mut self ) {
+        self.num += 1;
+        self.start = self.end + 1;
+        self.end += 8 * u32::from(self.num);
+        self.size += 2;
+    }
+    
+    fn for_square( square : u32 ) -> Ring {
+        let mut ring = Ring::zero();
+
+        while ring.end < square {
+            ring.next_ring();
+        }
+    
+        return ring;
+    }
+    
+    fn is_in_ring( &self, square : u32 ) -> bool {
+        return self.start <= square && square <= self.end;
+    }
+    
+    fn distance_from_middle_of_side( &self, square : u32 ) -> Result<i64, RingError> {
+        if !self.is_in_ring(square) {
+            return Err(RingError::NotInRing);
+        }
+        if square == 1 {
+            return Ok(0);
+        }
+        
+        let mut pos : i64 = i64::from(square) - i64::from(self.start) + 1;
+        pos %= i64::from(self.size) - 1;
+        pos -= (i64::from(self.size)-1)/2;
+        return Ok(pos.abs());
+    }
+    
+    fn manhattan_distance( square : u32 ) -> i64 {
+        let ring = Ring::for_square( square );
+        return ring.distance_from_middle_of_side( square ).unwrap()
+                + i64::from(ring.num);
+    }
+}
+
+fn main() {
+    let pos : u32 = env::args().nth(1).expect("square position")
+                               .parse().expect("positive integer");
+    println!("{}", Ring::manhattan_distance(pos));
+}
