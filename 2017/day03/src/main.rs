c77ed4a92cd70e6923bb17ed93416e504ef2e298
Michael G. Schwern <schwern@pobox.com>
2017-12-07 21:19:42 -0800

    Add command line parsing with clap.
    So we can do part 1 and part 2 with one binary.

--- 2017/day03/src/main.rs
+++ 2017/day03/src/main.rs
@@ -1,10 +1,46 @@
 extern crate day03;
-use std::env;
+
+#[macro_use]
+extern crate clap;
 
 use day03::Ring;
 
+fn parse_u32( input : &str ) -> u32 {
+    return input.parse().expect("positive integer");
+}
+
 fn main() {
-    let pos : u32 = env::args().nth(1).expect("square position")
-                               .parse().expect("positive integer");
-    println!("{}", Ring::manhattan_distance(pos));
+    let app_matches = clap_app!(day03 =>
+        (version: "1.0.0")
+        (author: "Michael G Schwern")
+        (about: "Advent Of Code 2017 Day 3")
+        (@subcommand one =>
+            (about: "Day 3 Part 1")
+            (@arg square: +required)
+        )
+        (@subcommand two =>
+            (about: "Day 3 Part 2")
+            (@arg square: +required)
+        )
+    ).get_matches();
+    
+    match app_matches.subcommand() {
+        ("one", Some(sub))   => {
+            let input :u32 = parse_u32( sub.value_of("square").unwrap() );
+            part_one(input);
+        },
+        ("two", Some(sub))   => {
+            let input :u32 = parse_u32( sub.value_of("square").unwrap() );
+            part_two(input);
+        },
+        _                    => println!("{}", app_matches.usage()),
+    }
+}
+
+fn part_one(input : u32) {
+    println!("{}", Ring::manhattan_distance(input));
+}
+
+fn part_two(input : u32) {
+    println!("input: {}", input);
 }
