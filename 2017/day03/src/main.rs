4a4410d1149fd750d66df52e6c0952a5e0e5273b
Michael G. Schwern <schwern@pobox.com>
2017-12-08 17:04:52 -0800

    Day 3 Part 2
    Add Point and Point.neighbors() to find all the neighbors of that point.
    
    Make Spiral use Point.

--- 2017/day03/src/main.rs
+++ 2017/day03/src/main.rs
@@ -4,6 +4,20 @@ extern crate day03;
 extern crate clap;
 
 use day03::ring;
+use day03::spiral;
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_first_larger_in_spiral() {
+        assert_eq!( first_larger_in_spiral(3), 4 );
+        assert_eq!( first_larger_in_spiral(747), 806 );
+        assert_eq!( first_larger_in_spiral(360), 362 );
+    }
+}
 
 fn parse_u32( input : &str ) -> u32 {
     return input.parse().expect("positive integer");
@@ -41,6 +55,32 @@ fn part_one(input : u32) {
     println!("{}", ring::Ring::manhattan_distance(input));
 }
 
+fn first_larger_in_spiral( input: u32 ) -> u32 {
+    let mut spiral = spiral::Spiral::new();
+    let mut grid : HashMap<spiral::Point,u32> = HashMap::new();
+
+    // Start with 1 at the center.
+    grid.insert(spiral.next().unwrap(), 1);
+
+    let zero :u32 = 0;
+    let mut val : u32 = 0;
+    for point in spiral {
+        val = 0;
+        for neighbor in point.neighbors() {
+            let nval = grid.get(&neighbor).unwrap_or(&zero);
+            val += nval;
+        }
+        
+        grid.insert(point, val);
+        
+        if val > input {
+            break;
+        }
+    }
+    
+    return val;
+}
+
 fn part_two(input : u32) {
-    println!("{}", input);
+    println!("{}", first_larger_in_spiral(input));
 }
--- 2017/day03/src/spiral.rs
+++ 2017/day03/src/spiral.rs
@@ -3,7 +3,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_spiral() {
         let wants = vec![
             (0,0),
             (1,0), (1,1),
@@ -15,7 +15,7 @@ mod tests {
             (-2,1), (-2,0), (-2,-1), (-2,-2),
             (-1,-2), (0,-2), (1,-2), (2,-2),
             (3,-2),
-        ];
+        ].into_iter().map( |a| Point{ x: a.0, y: a.1 } );
         
         let mut spiral = Spiral::new();
         
@@ -25,12 +25,76 @@ mod tests {
             assert_eq!( have, want );
         }
     }
+    
+    #[test]
+    fn test_point_neighbors() {
+        let mut want : Vec<Point> = vec![
+            (0, 2),  (1,2),  (2,2),
+            (0, 1),          (2,1),
+            (0, 0),  (1,0),  (2,0),
+        ].into_iter()
+         .map( |a| Point{ x: a.0, y: a.1 } )
+         .collect();
+        
+        let point = Point{ x: 1, y: 1 };
+        let mut neighbors : Vec<Point> = point.neighbors().collect();
+        
+        assert_eq!( neighbors.sort(), want.sort() );
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord, Hash)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    pub fn neighbors(&self) -> PointNeighbors {
+        const NEIGHBOR_MASK : [(i32,i32); 8] = [
+            (-1,1),  (0,1),  (1,1),
+            (-1,0),          (1,0),
+            (-1,-1), (0,-1), (1,-1),
+        ];
+        
+        return PointNeighbors{
+            point: self.clone(),
+            mask: NEIGHBOR_MASK,
+            pos: 0
+        };
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct PointNeighbors {
+    point: Point,
+    mask: [(i32,i32); 8],
+    pos: usize,
+}
+
+impl Iterator for PointNeighbors {
+    type Item = Point;
+    
+    fn next( &mut self ) -> Option<Point> {
+        if self.pos >= self.mask.len() {
+            return None;
+        }
+        
+        let mask = self.mask[self.pos];
+        let neighbor = Point{
+            x: self.point.x + mask.0,
+            y: self.point.y + mask.1,
+        };
+        
+        self.pos += 1;
+        
+        return Some(neighbor);
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Spiral {
-    x: i32,
-    y: i32,
+    pos: Point,
     ring: i32,
 }
 
@@ -40,39 +104,39 @@ impl Spiral {
         // next() be 0,0. If the next() logic
         // changes, this initial condition should
         // also change.
-        Spiral { ring: 0, x: -1, y: 0 }
+        Spiral { ring: 0, pos: Point{x: -1, y: 0} }
     }
 }
 
 impl Iterator for Spiral {
-    type Item = (i32,i32);
+    type Item = Point;
     
-    fn next(&mut self) -> Option<(i32,i32)> {
+    fn next(&mut self) -> Option<Point> {
         // Next ring
-        if self.x == self.ring && self.y == -self.ring {
+        if self.pos.x == self.ring && self.pos.y == -self.ring {
             self.ring += 1;
-            self.x += 1;
+            self.pos.x += 1;
         }
         // Bottom
-        else if self.y == -self.ring {
-            self.x += 1;
+        else if self.pos.y == -self.ring {
+            self.pos.x += 1;
         }
         // Left
-        else if self.x == -self.ring {
-            self.y -= 1;
+        else if self.pos.x == -self.ring {
+            self.pos.y -= 1;
         }
         // Top
-        else if self.y == self.ring {
-            self.x -= 1;
+        else if self.pos.y == self.ring {
+            self.pos.x -= 1;
         }
         // Right
-        else if self.x == self.ring {
-            self.y += 1;
+        else if self.pos.x == self.ring {
+            self.pos.y += 1;
         }
         else {
             panic!(format!("Spiral should not be here: {:?}", self));
         }
         
-        return Some((self.x, self.y))
+        return Some(self.pos.clone())
     }
 }
