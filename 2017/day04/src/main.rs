205f311020ebe047db3bbbee91f9a2447a7820ec
Michael G. Schwern <schwern@pobox.com>
2017-12-08 22:59:20 -0800

    Day 4 Part 2

--- 2017/day04/src/main.rs
+++ 2017/day04/src/main.rs
@@ -21,12 +21,44 @@ mod tests {
             assert_eq!( is_valid_password1(arg), want );
         }
     }
+    
+    #[test]
+    fn test_is_valid_password2() {
+        let tests = hashmap!{
+            "abcde fghij"       => true,
+            "abcde xyz ecdab"   => false,
+            "a ab abc abd abf abj" => true,
+            "iiii oiii ooii oooi oooo" => true,
+            "oiii ioii iioi iiio" => false,
+        };
+        for (arg,want) in tests {
+            assert_eq!( is_valid_password2(arg), want );
+        }
+    }
 }
 
 fn is_valid_password1( password : &str ) -> bool {
     let mut words : HashMap<&str,usize> = hashmap!{};
     for word in password.split(" ") {
         let count = words.entry(word).or_insert(0);
+        *count += 1;
+        
+        if *count > 1 {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+fn is_valid_password2( password : &str ) -> bool {
+    let mut anagrams : HashMap<String,usize> = hashmap!{};
+    for word in password.split(" ") {
+        let mut chars : Vec<char> = word.chars().collect();
+        chars.sort();
+        let anagram : String = chars.into_iter().collect();
+        
+        let count = anagrams.entry(anagram).or_insert(0);
         *count += 1;
         
         if *count > 1 {
@@ -44,7 +76,7 @@ fn main() {
     
     let mut num_valid = 0;
     for line in reader.lines() {
-        if is_valid_password1( &line.unwrap() ) {
+        if is_valid_password2( &line.unwrap() ) {
             num_valid += 1;
         }
     }
